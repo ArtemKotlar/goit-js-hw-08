8c0fd4a3f0d8ca4a9916859604740e8e6f481752
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-22 09:02:20 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const showSeconds = throttle(({ seconds }) => {
+  console.log('seconds', seconds);
+}, 1000);
+
+player.on('timeupdate', showSeconds);
+player.setCurrentTime(30.456);
