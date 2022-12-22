26a678ed20980236ca245ec08b40742c5043480d
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-22 20:14:06 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,33 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery">
+        <a class="gallery__item" href = ${original}>
+            <img 
+                class = "gallery__image"
+                src = ${preview} 
+                alt = ${description}
+            />
+        </a>
+    </li>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
