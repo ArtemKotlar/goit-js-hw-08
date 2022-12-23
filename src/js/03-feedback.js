90d9ef67d39cdfd927c04c1376c6c4e572dc88f7
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-23 16:28:22 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import { throttle } from 'lodash';
 
+// player.on('timeupdate', showSeconds);
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+function onFormData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+}
+
+function onSubmitForm(evt) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
