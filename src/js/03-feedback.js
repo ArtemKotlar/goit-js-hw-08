dc8e0d2f85200c8cc49d9e721d8a162f376442b1
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-27 17:43:30 +0200

    Зауваження

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,27 +6,26 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
+let formData = {};
 
 function onFormData(evt) {
   formData[evt.target.name] = evt.target.value;
-  localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(evt) {
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   evt.preventDefault();
   evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
 
 function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  formData = data;
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
-  if (data === null) {
-    return;
-  }
+
   email.value = data.email || '';
   message.value = data.message || '';
 }
